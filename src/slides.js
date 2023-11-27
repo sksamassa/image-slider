fcc2b7d0715782dd0a5563dcf396eae1e716f443
Sekou Samassa <sksamassa@gmail.com>
2023-11-28 01:44:36 +0500

    Project completed

---
+++ src/slides.js
@@ -0,0 +1,28 @@
+import image1 from './image-1.jpg';
+import image2 from './image-2.jpg';
+import image3 from './image-3.jpg';
+import image4 from './image-4.jpg';
+import image5 from './image-5.jpg';
+
+export const slides = [
+    {
+        url: image1,
+        title: 'Beach'
+    },
+    {
+        url: image2,
+        title: 'Boat'
+    },
+    {
+        url: image3,
+        title: 'Forests'
+    },
+    {
+        url: image4,
+        title: 'City'
+    },
+    {
+        url: image5,
+        title: 'Italy'
+    }
+];
